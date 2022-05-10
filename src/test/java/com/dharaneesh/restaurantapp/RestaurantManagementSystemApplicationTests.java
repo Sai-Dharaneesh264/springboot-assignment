97a363ad6b698a5ec59877fb01cd3164adfe5126
Sai-Dharaneesh264 <saidharaneesh.gurram@zemosolabs.com>
2022-05-10 14:26:44 +0530

    updated test file

--- src/test/java/com/dharaneesh/restaurantapp/RestaurantManagementSystemApplicationTests.java
+++ src/test/java/com/dharaneesh/restaurantapp/RestaurantManagementSystemApplicationTests.java
@@ -1,18 +1,11 @@
 package com.dharaneesh.restaurantapp;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.ui.Model;
 
 @SpringBootTest
 class RestaurantManagementSystemApplicationTests {
 
 
-    @Mock
-    private Model model;
-    @Test
-    void contextLoads() {
-    }
 
 }
