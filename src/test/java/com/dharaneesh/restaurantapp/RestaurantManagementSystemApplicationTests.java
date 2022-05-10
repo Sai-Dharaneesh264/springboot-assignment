68afd0a58b0d2ad4ebf929728454d901819077a8
Sai-Dharaneesh264 <saidharaneesh.gurram@zemosolabs.com>
2022-05-10 15:35:20 +0530

    added first test case

--- src/test/java/com/dharaneesh/restaurantapp/RestaurantManagementSystemApplicationTests.java
+++ src/test/java/com/dharaneesh/restaurantapp/RestaurantManagementSystemApplicationTests.java
@@ -1,11 +1,20 @@
 package com.dharaneesh.restaurantapp;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import com.dharaneesh.restaurantapp.dto.Name;
+import org.mockito.Mockito;
 @SpringBootTest
 class RestaurantManagementSystemApplicationTests {
 
 
+    @Test
+    public void checkName() {
+        Name name = new Name("sai", "dharaneesh");
+        Assert.assertEquals(name.getFirstName(), "sai");
+        Assert.assertEquals(name.getLastName(), "dharaneesh");
+    }
 
 }
